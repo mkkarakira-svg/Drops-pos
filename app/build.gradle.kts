8d6ef7cbefea112f143e3c446ce2aeda9f007632
mkkarakira-svg <mk.karakira@gmail.com>
2026-09-21 10:22:14 +0300

    Bump app version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.drops.pos.v1"
         minSdk = 23
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.0.0"
+        versionCode = 10
+        versionName = "2.1.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
