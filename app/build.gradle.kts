b406551c2d93e8d5351f8797b77454f1241669af
mkkarakira-svg <mk.karakira@gmail.com>
2026-09-21 14:14:13 +0300

    Bump final test build to 3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ android {
         applicationId = "com.drops.pos.v1"
         minSdk = 23
         targetSdk = 35
-        versionCode = 12
-        versionName = "3.1.0"
+        versionCode = 13
+        versionName = "3.2.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
