9017957a27a341022a71942a01c885eebe704a32
mkkarakira-svg <mk.karakira@gmail.com>
2026-09-23 17:46:48 +0300

    Bump web cache v129

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='drops-pos-v128';
+const C='drops-pos-v129';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
